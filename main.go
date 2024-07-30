89fac41ff07e0691960c37e4fa382458ab65ec6c
Nabin Katwal <nabinkatwal2000@gmail.com>
2024-07-30 14:43:48 +0545

    util: add cors

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/nabinkatwal7/go-rbac/controller"
 	"github.com/nabinkatwal7/go-rbac/db"
@@ -13,6 +14,10 @@ import (
 
 func serveApplication() {
 	router := gin.Default()
+
+	config := cors.DefaultConfig()
+	//config.AllowOrigins = []string{"http://localhost:4200"}
+	router.Use(cors.New(config))
 
 	authRoutes := router.Group("/auth/user")
 	authRoutes.POST("/register", controller.Register)
